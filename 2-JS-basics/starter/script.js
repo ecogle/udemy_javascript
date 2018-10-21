b9f3641f247493b91affaee82e520fb4ea25885c
Chad Ogle <ecogle@c3devloping.com>
2018-10-21 05:56:34 -0500

    type coercion and var mutation along with alert and prompt

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,4 @@
+/*
 var firstName = 'John'; //Single quotes or double quotes
 console.log(firstName);
 
@@ -12,3 +13,30 @@ console.log(job);
 
 job='Teacher';
 console.log(job);
+*/
+
+/************************************************************
+* Variable Mutation and type coercion
+*/
+
+var firstName = 'John';
+var age=28;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+
+job = 'Teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name');
+console.log(firstName + ' ' + lastName);
